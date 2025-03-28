24adce7727ca1387091b5b02dec2abfe0d7d39da
Raphael Dimeck <raphael.dimeck@gmail.com>
2025-03-28 17:25:20 +0100

    CLEAN

--- app/src/main/java/app/insight/activity/MainActivity.java
+++ app/src/main/java/app/insight/activity/MainActivity.java
@@ -2,7 +2,6 @@ package app.insight.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
